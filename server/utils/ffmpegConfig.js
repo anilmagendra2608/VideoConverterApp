97121c073a0352c07d3086e4d50b0bc52f2166ed
Anil Magendra <anil.magene@gmail.com>
2024-09-01 12:25:49 +1000

    Adding updated config and dockerfile

--- server/utils/ffmpegConfig.js
+++ server/utils/ffmpegConfig.js
@@ -1,7 +1,18 @@
+/*
+This is for Windows system 
 const ffmpeg = require("fluent-ffmpeg");
 
 module.exports = function () {
   ffmpeg.setFfmpegPath("C:/ffmpeg/ffmpeg-7.0.2-full_build/bin/ffmpeg.exe");
   ffmpeg.setFfprobePath("C:/ffmpeg/ffmpeg-7.0.2-full_build/bin");
   ffmpeg.setFlvtoolPath("C:/flvtool");
+};
+*/
+
+const ffmpeg = require("fluent-ffmpeg");
+
+module.exports = function () {
+  ffmpeg.setFfmpegPath("/usr/bin/ffmpeg");
+  ffmpeg.setFfprobePath("/usr/bin/ffprobe");
+  ffmpeg.setFlvtoolPath("/path/to/flvtool");
 };
